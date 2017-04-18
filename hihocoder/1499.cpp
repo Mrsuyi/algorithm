a3f7a0391dddbcc062d5057f9e2e04cc3c6f68fa
Su <suyi05@internal.baidu.com>
2017-04-18 18:02:10 +0800

    t_t

---
+++ hihocoder/1499.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+typedef long long ll;
+
+const int MAXN = 100001;
+int a[MAXN];
+int b[MAXN];
+
+int main()
+{
+	int N, avg = 0;
+	cin >> N;
+	for (int i = 0; i < N; ++i)
+	{
+		cin >> a[i] >> b[i];
+		avg += a[i] + b[i];
+	}
+	avg /= 2 * N;
+	ll res = 0, p = 0, q = 0;
+	for (int i = 0; i < N; ++i)
+	{
+		p += avg - a[i];
+		q += avg - b[i];
+		if ((p ^ q) & (1 << 63))
+		{
+			res += min(abs(p), abs(q));
+			if (abs(p) > abs(q))
+			{
+				p += q;
+				q = 0;
+			}
+			else
+			{
+				q += p;
+				p = 0;
+			}
+		}
+		res += abs(p) + abs(q);
+	}
+	cout << res << endl;
+	
+	return 0;
+};
