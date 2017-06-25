2ef99b7fec68cf351f4a4e37be9c9bfadcd47765
Mrsuyi <mrsuyi@163.com>
2017-06-26 00:44:58 +0800

    x_x

---
+++ codeforce/420-div2/b.cpp
@@ -0,0 +1,64 @@
+#include <algorithm>
+#include <cfloat>
+#include <climits>
+#include <cstring>
+#include <cmath>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <vector>
+# ifdef __GNUC__
+# if __cplusplus > 199711L
+# include <unordered_set>
+# include <unordered_map>
+# else
+# include <tr1/unordered_map>
+# include <tr1/unordered_set>
+using namespace tr1;
+# endif
+# else
+# include <unordered_map>
+# include <unordered_set>
+# endif
+using namespace std;
+
+#define fi first
+#define se second
+#define FO(x, n) for (int x = 0; x < n; ++x)
+#define FOR(x, a, b) for (int x = a; x < b; ++x)
+#define RFO(x, n) for (int x = n - 1; x >= 0; --x)
+#define RFOR(x, a, b) for (int x = b - 1; x >= a; --x)
+typedef unsigned char byte;
+typedef unsigned int uint;
+typedef long long llong;
+typedef unsigned long long ullong;
+typedef pair<int, int> pii;
+typedef pair<double, double> pdd;
+typedef pair<llong, llong> pll;
+inline bool feq(const double& a, const double& b) { return fabs(a - b) < 1e-10; }
+
+llong M, B;
+
+int main()
+{
+    llong res;
+    while (cin >> M >> B)
+    {
+        res = 0;
+        for (llong x = 0; x <= M * B; ++x)
+        {
+            llong y = B - double(x) / M;
+            llong cur = (1 + x) * x / 2 * (y + 1) + (1 + y) * y / 2 * (x + 1);
+            res = max(res, cur);
+        }
+        cout << res << endl;
+    }
+    return 0;
+}
