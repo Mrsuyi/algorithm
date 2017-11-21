a0249138687405513657738fdacd33785df3e73e
Mrsuyi <mrsuyi@163.com>
2017-11-21 10:30:17 +0800

    11572

---
+++ uva/11572.cpp
@@ -0,0 +1,70 @@
+#include <algorithm>
+#include <array>
+#include <cfloat>
+#include <climits>
+#include <cstring>
+#include <cmath>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <vector>
+# ifdef __GNUC__
+# if __cplusplus > 199711L
+# include <unordered_set>
+# include <unordered_map>
+# else
+# include <tr1/unordered_map>
+# include <tr1/unordered_set>
+using namespace tr1;
+# endif
+# else
+# include <unordered_map>
+# include <unordered_set>
+# endif
+using namespace std;
+
+#define FO(x, n) for (int x = 0; x < n; ++x)
+typedef long long llong;
+typedef pair<int, int> pii;
+typedef pair<double, double> pdd;
+typedef pair<llong, llong> pll;
+const int INF = 0x0f0f0f0f, MOD = 1e9 + 7, EPS = 1e-8;
+
+inline bool feq(const double& a, const double& b) { return fabs(a - b) < EPS; }
+
+const int MAXN = 1000100;
+int N;
+int nums[MAXN];
+
+int main()
+{
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+    int T;
+    cin >> T;
+    FO (t, T)
+    {
+        cin >> N;
+        FO (i, N)
+            scanf("%d", &nums[i]);
+        unordered_map<int, int> cnt;
+        int l = 0, res = 0;
+        FO (i, N)
+        {
+            ++cnt[nums[i]];
+            while (cnt[nums[i]] > 1)
+                --cnt[nums[l++]];
+            res = max(res, i - l + 1);
+        }
+        printf("%d\n", res);
+    }
+    return 0;
+}
