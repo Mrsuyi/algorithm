718b9ca05d48c17520148117ecc7387f6b2ba3d4
Mrsuyi <mrsuyi@163.com>
2018-03-19 20:24:30 +0800

    .$

--- uva/10934.cpp
+++ uva/10934.cpp
@@ -41,26 +41,26 @@ typedef pair<llong, llong> pll;
 const int INF = 0x7f7f7f7f, MOD = 1e9 + 7, EPS = 1e-8;
 inline bool feq(const double& a, const double& b) { return fabs(a - b) < EPS; }
 
-const int MAXK = 110;
-llong dp[110][1010] = { 0 };
+const int MAXK = 110, MAXM = 70;
+llong dp[MAXK][MAXM] = {0};
+llong K, N;
 
 int main()
 {
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
 
-    for (int i = 1; i <= 100; ++i)
+    for (int i = 1; i < MAXK; ++i)
+    for (int j = 1; j < MAXM; ++j)
+        dp[i][j] = dp[i - 1][j - 1] + dp[i][j - 1] + 1;
+
+    while (cin >> K >> N && K)
     {
-        for (int j = 1; j <= 1000; ++j)
-        {
-            dp[i][j] = LLONG_MAX;
-            for (int k = 1; k <= j; ++k)
-            {
-                dp[i][j] = min(dp[i][j], 1 + max(dp[i - 1][k - 1], dp[i][j - k]));
-            }
-            cout << dp[i][j] << " ";
-        }
-        cout << endl;
+        llong* it = lower_bound(&dp[K][1], &dp[K][64], N);
+        if (it == &dp[K][64])
+            printf("More than 63 trials needed.\n");
+        else
+            printf("%d\n", it - &dp[K][0]);
     }
     return 0;
 }
