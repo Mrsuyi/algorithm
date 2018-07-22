03581b0e9d3f4b53c69315ecfa42e1778646c0ad
Yi Su <mrsuyi@google.com>
2018-07-22 21:45:08 +0200

    11582

--- uva/11582.cpp
+++ uva/11582.cpp
@@ -39,3 +39,41 @@ typedef pair<llong, llong> pll;
 const int MOD = 1e9 + 7;
 const double PI = 3.141592653589793238463, EPS = 1e-8;
 inline bool feq(const double& a, const double& b) { return fabs(a - b) < EPS; }
+
+ullong a, b, n;
+ullong rems[1010][7000];
+ullong t[1010];
+
+int main()
+{
+    for (int i = 1; i <= 1000; ++i)
+    {
+        rems[i][0] = 0;
+        rems[i][1] = 1 % i;
+        for (int j = 2;; ++j)
+        {
+            rems[i][j] = (rems[i][j - 1] + rems[i][j - 2]) % i;
+            if (rems[i][j - 1] == rems[i][0] && rems[i][j] == rems[i][1])
+            {
+                t[i] = j - 1;
+                break;
+            }
+        }
+    }
+
+    int O;
+    cin >> O;
+    FO(o, O)
+    {
+        cin >> a >> b >> n;
+        ullong pos = 1, div = a % t[n];
+        while (b)
+        {
+            if (b & 1) pos = (pos * div) % t[n];
+            div = (div * div) % t[n];
+            b >>= 1;
+        }
+        cout << rems[n][pos] << endl;
+    }
+    return 0;
+}
