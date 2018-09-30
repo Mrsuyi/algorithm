93367b08b109ab0e3bbc74398f92e11f70a775e7
Mrsuyi <mrsuyi@163.com>
2018-09-30 18:08:35 +0200

    u_u

--- templates.cpp
+++ templates.cpp
@@ -38,54 +38,78 @@ typedef pair<double, double> pdd;
 typedef pair<llong, llong> pll;
 const int MOD = 1e9 + 7;
 const double PI = 3.141592653589793238463, EPS = 1e-8;
-inline bool feq(const double& a, const double& b) { return fabs(a - b) < EPS; }
+inline bool feq(const double& a, const double& b) {
+  return fabs(a - b) < EPS;
+}
 
 // a > b
 // gcd of a/b
-int gcd(int a, int b) { return b ? gcd(b, a % b) : a; }
+int gcd(int a, int b) {
+  return b ? gcd(b, a % b) : a;
+}
 
 // a > b
 // d = gcd(a, b), a * x + b * y = d
-void exgcd(int a, int b, int& d, int& x, int& y)
-{
-    if (b) {
-        exgcd(b, a % b, d, y, x);
-        y -= x * (a / b);
-    } else {
-        d = a, x = 1, y = 0;
-    }
+void exgcd(int a, int b, int& d, int& x, int& y) {
+  if (b) {
+    exgcd(b, a % b, d, y, x);
+    y -= x * (a / b);
+  } else {
+    d = a, x = 1, y = 0;
+  }
 }
 
 // Eratosthenes filtering
-// get all primes in [0, n]
+// get all primes in [0, n].
 bool not_prime[10000] = {0};
 vector<int> primes;
-void get_primes(int n)
-{
-    int m = sqrt(n + 0.5);
-    for (int i = 2; i <= m; ++i) {
-        if (not_prime[i]) continue;
-        for (int j = i * i; j <= n; j += i) not_prime[j] = true;
-    }
-    for (int i = 2; i <= n; ++i)
-        if (!not_prime[i]) primes.push_back(i);
+void get_primes(int n) {
+  int m = sqrt(n + 0.5);
+  for (int i = 2; i <= m; ++i) {
+    if (not_prime[i])
+      continue;
+    for (int j = i * i; j <= n; j += i)
+      not_prime[j] = true;
+  }
+  for (int i = 2; i <= n; ++i)
+    if (!not_prime[i])
+      primes.push_back(i);
 }
 
-// unique prime-factorization theorem
-vector<pii> factorize(int n)
-{
-    vector<pii> res;
-    for (size_t i = 0; i < primes.size() && primes[i] <= n && n > 1; ++i) {
-        if (n % primes[i] == 0) {
-            res.push_back({primes[i], 0});
-            while (n % primes[i] == 0) {
-                ++res.back().second;
-                n /= primes[i];
-            }
-        }
+// Unique prime-factorization theorem
+// factorize a natural number into a mutiplication of prime numbers.
+vector<pii> factorize(int n) {
+  vector<pii> res;
+  for (size_t i = 0; i < primes.size() && primes[i] <= n && n > 1; ++i) {
+    if (n % primes[i] == 0) {
+      res.push_back({primes[i], 0});
+      while (n % primes[i] == 0) {
+        ++res.back().second;
+        n /= primes[i];
+      }
     }
-    if (n > 1) res.push_back({n, 1});
-    return res;
+  }
+  if (n > 1)
+    res.push_back({n, 1});
+  return res;
 }
 
-int main() { return 0; }
+// Euler Phi function
+// number of m in [1, N] that is coprime to N.
+int phi[10000] = {0};
+void euler_phi() {
+  phi[1] = 1;
+  for (int i = 2; i < 10000; ++i) {
+    if (phi[i])
+      continue;
+    for (int j = i; j < 10000; j += i) {
+      if (!phi[j])
+        phi[j] = j;
+      phi[j] = phi[j] * (i - 1) / i;
+    }
+  }
+}
+
+int main() {
+  return 0;
+}
