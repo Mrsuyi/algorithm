a757e8f40d4a4d0d59d60be09e80cdf53aca6902
Mrsuyi <mrsuyi@163.com>
2018-10-01 21:08:28 +0200

    o_o

---
+++ uva/10491.cpp
@@ -0,0 +1,58 @@
+#include <algorithm>
+#include <array>
+#include <cfloat>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <vector>
+#ifdef __GNUC__
+#if __cplusplus > 199711L
+#include <unordered_map>
+#include <unordered_set>
+#else
+#include <tr1/unordered_map>
+#include <tr1/unordered_set>
+using namespace tr1;
+#endif
+#else
+#include <unordered_map>
+#include <unordered_set>
+#endif
+using namespace std;
+#define FO(x, n) for (int x = 0; x < n; ++x)
+#define RFO(x, n) for (int x = n - 1; x >= 0; --x)
+typedef unsigned int uint;
+typedef long long llong;
+typedef unsigned long long ullong;
+typedef pair<int, int> pii;
+typedef pair<double, double> pdd;
+typedef pair<llong, llong> pll;
+const int MOD = 1e9 + 7;
+const double PI = 3.141592653589793238463, EPS = 1e-8;
+inline bool feq(const double& a, const double& b) {
+  return fabs(a - b) < EPS;
+}
+
+int main() {
+  double cow, car, show;
+  while (cin >> cow >> car >> show) {
+    double tot = cow + car;
+    double res = 0;
+    double pcow = cow / tot;
+    res += pcow * car / (tot - 1 - show);
+    double pcar = car / tot;
+    res += pcar * (car - 1) / (tot - 1 - show);
+    printf("%.5lf\n", res);
+  }
+  return 0;
+}
