f1d7831a36fd91007d6e90fa5bd7fcac08d049b3
Mrsuyi <mrsuyi@163.com>
2019-06-24 22:56:16 +0200

    wryyyyyyyy!!!!!!!!!!!!

--- README.md
+++ README.md
@@ -7,41 +7,41 @@
 
 # problems
 
-## 贪心
+## Greedy
 + uva-1442 扫描 <https://vjudge.net/problem/UVA-1442>
 + uva-1614 归纳证明 <https://vjudge.net/problem/UVA-1614>
 + uva-1153 区间排序+优先队列 <https://vjudge.net/problem/UVA-1153>
 + uva-1617 区间排序 <https://vjudge.net/problem/UVA-1617>
-+ uva-1623 二分查找/下标数组 <https://vjudge.net/problem/UVA-1623>
++ uva-1623 map/union-find <https://vjudge.net/problem/UVA-1623>
 
-## 暴力
+## brute-force
 + uva-1312 排序枚举 <https://vjudge.net/problem/UVA-1312>
 
-## 博弈
+## game
 + uva-1609 递归+构造 <https://vjudge.net/problem/UVA-1609>
 
-## 分治
+## divide-conquer
 + uva-12627 找规律 <https://vjudge.net/problem/UVA-12627>
 + uva-1608 两端扫描 <https://vjudge.net/problem/UVA-1608>
 
-## 单调栈
+## singular-stack
 + uva-12265 <https://vjudge.net/problem/UVA-12265>
 + uva-1619 分治/排序/单调栈 <https://vjudge.net/problem/UVA-1619>
 
-## 回溯
+## back-track
 + uva-140 剪枝 <https://vjudge.net/problem/UVA-140>
 + uva-1354 子集遍历 <https://vjudge.net/problem/UVA-1354>
 
-## 二分
+## binary-search
 + uva-1607 模拟 <https://vjudge.net/problem/UVA-1607>
 
-## 图论
-### BFS/DFS/IDA*
+## graph
+### BFS/DFS/IDA\*
 + uva-11853 建图 <https://vjudge.net/problem/UVA-11853>
 + uva-1599 反向BFS <https://vjudge.net/problem/UVA-1599>
 + uva-10410 栈 <https://vjudge.net/problem/UVA-10410>
 + uva-1601 双向BFS+建图剪枝 <https://vjudge.net/problem/UVA-1601>
-+ uva-11212 IDA* <https://vjudge.net/problem/UVA-11212>
++ uva-11212 IDA\* <https://vjudge.net/problem/UVA-11212>
 + uva-818 统计链数量 <https://vjudge.net/problem/UVA-818>
 + uva-12107 双DFS <https://vjudge.net/problem/UVA-12107>
 + uva-12569 状态压缩BFS <https://vjudge.net/problem/UVA-12569>
@@ -49,7 +49,7 @@
 ### Euler
 + uva-12118 连通块 <https://vjudge.net/problem/UVA-12118>
 
-## 计算几何
+## geometry
 + uva-1606 极角排序+扫描 <https://vjudge.net/problem/UVA-1606>
 + uva-1451 斜率优化 <https://vjudge.net/problem/UVA-1451>
 
--- uva/1623.cpp
+++ uva/1623.cpp
@@ -1,10 +1,9 @@
 #include <algorithm>
 #include <array>
-#include <cassert>
 #include <cfloat>
 #include <climits>
-#include <cstring>
 #include <cmath>
+#include <cstring>
 #include <deque>
 #include <functional>
 #include <iostream>
@@ -16,236 +15,104 @@
 #include <stack>
 #include <string>
 #include <vector>
-# ifdef __GNUC__
-# if __cplusplus > 199711L
-# include <unordered_set>
-# include <unordered_map>
-# else
-# include <tr1/unordered_map>
-# include <tr1/unordered_set>
+#ifdef __GNUC__
+#if __cplusplus > 199711L
+#include <unordered_map>
+#include <unordered_set>
+#else
+#include <tr1/unordered_map>
+#include <tr1/unordered_set>
 using namespace tr1;
-# endif
-# else
-# include <unordered_map>
-# include <unordered_set>
-# endif
+#endif
+#else
+#include <unordered_map>
+#include <unordered_set>
+#endif
 using namespace std;
 #define FO(x, n) for (int x = 0; x < n; ++x)
 #define RFO(x, n) for (int x = n - 1; x >= 0; --x)
+typedef unsigned int uint;
 typedef long long llong;
+typedef unsigned long long ullong;
+typedef long double ldouble;
 typedef pair<int, int> pii;
 typedef pair<double, double> pdd;
 typedef pair<llong, llong> pll;
-const int INF = 0x7fffff7f, MOD = 1e9 + 7, EPS = 1e-8;
-inline bool feq(const double& a, const double& b) { return fabs(a - b) < EPS; }
+const int MOD = 1e9 + 7;
+const double PI = 3.141592653589793238463, EPS = 1e-8;
+inline bool feq(const double& a, const double& b) {
+  return fabs(a - b) < EPS;
+}
 
 template <class T>
-void read(T& res)
-{
-    res = 0;
-    char c = getchar();
-    while (c < '0') c = getchar();
-    while (c >= '0') res = 10 * res + c - '0', c = getchar();
+void read(T& res) {
+  res = 0;
+  char c = getchar();
+  while (c < '0')
+    c = getchar();
+  while (c >= '0')
+    res = 10 * res + c - '0', c = getchar();
 }
 
-// const int MAXN = 1000100;
-// int N, M;
-// int full[MAXN], days[MAXN];
-// queue<int> ques[MAXN];
+int input[1000100];
+int nxt0[1000100];
+int pre_dup[1000100];
+int res[1000100];
 
-// int main()
-// {
-//     freopen("input.txt", "r", stdin);
-//     freopen("output.txt", "w", stdout);
-
-//     int O;
-//     cin >> O;
-//     FO (o, O)
-//     {
-//         cin >> N >> M;
-//         for (int i = 0; i < M; ++i)
-//             read(days[i]);
-//         for (int i = 1; i <= N; ++i)
-//             ques[i] = queue<int>();
-//         for (int i = 0; i < M; ++i)
-//             if (days[i])
-//                 ques[days[i]].push(i);
-//         map<int, int> can;
-//         for (int i = 1; i <= N; ++i)
-//             if (!ques[i].empty())
-//             {
-//                 can[ques[i].front()] = i;
-//                 ques[i].pop();
-//             }
-//         bool res = true;
-//         vector<int> sucks;
-//         sucks.reserve(M);
-//         for (int i = 0; i < M; ++i)
-//         {
-//             if (days[i])
-//             {
-//                 if (full[days[i]])
-//                 {
-//                     res = false;
-//                     break;
-//                 }
-//                 else
-//                 {
-//                     full[days[i]] = 1;
-//                     if (!ques[days[i]].empty())
-//                     {
-//                         can[ques[days[i]].front()] = days[i];
-//                         ques[days[i]].pop();
-//                     }
-//                 }
-//             }
-//             else
-//             {
-//                 if (!can.empty())
-//                 {
-//                     full[can.begin()->second] = 0;
-//                     sucks.push_back(can.begin()->second);
-//                     can.erase(can.begin());
-//                 }
-//                 else
-//                 {
-//                     sucks.push_back(0);
-//                 }
-//             }
-//         }
-//         if (res)
-//         {
-//             printf("YES\n");
-//             for (int i = 0; i < sucks.size(); ++i)
-//             {
-//                 if (i) printf(" ");
-//                 printf("%d", sucks[i]);
-//             }
-//             printf("\n");
-//         }
-//         else
-//             printf("NO\n");
-//     }
-//     return 0;
-// }
-
-
-// const int MAXN = 1000100;
-// int N, M;
-// int days[MAXN], lst[MAXN];
-
-// int main()
-// {
-//     freopen("input.txt", "r", stdin);
-//     freopen("output.txt", "w", stdout);
-
-//     int O;
-//     cin >> O;
-//     FO (o, O)
-//     {
-//         cin >> N >> M;
-//         map<int, int> st;
-//         for (int i = 0; i < M; ++i)
-//         {
-//             read(days[i]);
-//             if (!days[i])
-//                 st[i] = st.size();
-//         }
-//         memset(lst, -1, sizeof(lst));
-//         bool res = true;
-//         vector<int> sucks(st.size(), 0);
-//         for (int i = 0; i < M; ++i)
-//         {
-//             if (!days[i])
-//                 continue;
-//             auto it = st.upper_bound(lst[days[i]]);
-//             if (it == st.end() || it->first > i)
-//             {
-//                 res = false;
-//                 break;
-//             }
-//             sucks[it->second] = days[i];
-//             st.erase(it);
-//             lst[days[i]] = i;
-//         }
-//         if (res)
-//         {
-//             printf("YES\n");
-//             for (int i = 0; i < sucks.size(); ++i)
-//             {
-//                 if (i) printf(" ");
-//                 printf("%d", sucks[i]);
-//             }
-//             printf("\n");
-//         }
-//         else
-//             printf("NO\n");
-//     }
-//     return 0;
-// }
-
-
-const int MAXN = 1000100;
-int N, M;
-int f[MAXN], a[MAXN], ans[MAXN], lake[MAXN];
-
-int find(int x)
-{
-    return (f[x] == x) ? f[x] : f[x] = find(f[x]);
+int get_nxt0(int i) {
+  if (nxt0[i] == i || nxt0[i] == -1)
+    return nxt0[i];
+  return nxt0[i] = get_nxt0(nxt0[i]);
 }
 
-int main()
-{
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-
-    int O;
-    cin >> O;
-    FO (o, O)
-    {
-        cin >> N >> M;
-        map<int, int> st;
-        for (int i = 1; i <= M; ++i)
-        {
-            ans[i] = 0;
-            read(a[i]);
-        }
-        int lst = M + 1;
-        f[lst] = lst;
-        for (int i = M; i >= 1; --i)
-        {
-            if (!a[i])
-                lst = i;
-            f[i] = lst;
-        }
-        f[0] = lst;
-        bool res = true;
-        for (int i = 1; i <= N; ++i)
-            lake[i] = 0;
-        for (int i = 1; i <= M; ++i)
-        {
-            if (!a[i])
-                continue;
-            int x = find(lake[a[i]]);
-            if (x <= i)
-                ans[x] = a[i], f[x] = find(x + 1);
-            else
-            {
-                res = false;
-                break;
-            }
-            lake[a[i]] = i;
-        }
-        if (res)
-        {
-            printf("YES\n");
-            for (int i = 1; i <= M; ++i)
-                if (!a[i])
-                    printf("%d ", ans[i]);
-            printf("\n");
-        }
-        else
-            printf("NO\n");
+int main() {
+  int Z;
+  scanf("%d", &Z);
+  for (int z = 0; z < Z; ++z) {
+    int n, m;
+    scanf("%d %d", &n, &m);
+    int bytes = sizeof(int) * (m + 10);
+    for (int i = 0; i < m; ++i) {
+      read(input[i]);
     }
-    return 0;
+    memset(nxt0, -1, bytes);
+    for (int i = m - 1; i >= 0; --i) {
+      if (input[i] == 0) {
+        nxt0[i] = i;
+      } else {
+        nxt0[i] = nxt0[i + 1];
+      }
+    }
+    memset(res, 0, bytes);
+    memset(pre_dup, -1, bytes);
+    bool ok = true;
+    for (int i = 0; i < m; ++i) {
+      if (input[i] == 0)
+        continue;
+      int pd = pre_dup[input[i]];
+      int n0 = get_nxt0(pd + 1);
+      if (n0 > i || n0 == -1) {
+        ok = false;
+        break;
+      }
+      res[n0] = input[i];
+      nxt0[n0] = get_nxt0(n0 + 1);
+      pre_dup[input[i]] = i;
+    }
+
+    if (ok) {
+      printf("YES\n");
+      for (int i = 0; i < m; ++i) {
+        if (input[i] != 0)
+          continue;
+        if (i)
+          printf(" ");
+        printf("%d", res[i]);
+      }
+      printf("\n");
+    } else {
+      printf("NO\n");
+    }
+  }
+  return 0;
 }
