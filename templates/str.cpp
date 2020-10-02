8c761a920ce23b3885dd84ad032d8b677ac96584
Mrsuyi <mrsuyi@163.com>
2020-10-02 13:50:06 -0700

    re-format

--- templates/str.cpp
+++ templates/str.cpp
@@ -7,126 +7,126 @@ using namespace std;
 
 namespace kmp {
 
-  int n;
-  string pat;
-  vector<int> pre;
-  int state;
+int n;
+string pat;
+vector<int> pre;
+int state;
 
-  void init(const string& pattern) {
-    pat = pattern;
-    n = pat.size();
-    pre = vector<int>(n, 0);
-    for (int i = 1; i < n; ++i) {
-      pre[i] = pre[i - 1];
-      while (true) {
-        if (pat[i] == pat[pre[i]]) {
-          ++pre[i];
-          break;
-        }
-        if (pre[i] == 0)
-          break;
-        pre[i] = pre[pre[i] - 1];
-      }
-    }
-    state = 0;
-  }
-
-  bool match(char c) {
-    bool res = false;
+void init(const string &pattern) {
+  pat = pattern;
+  n = pat.size();
+  pre = vector<int>(n, 0);
+  for (int i = 1; i < n; ++i) {
+    pre[i] = pre[i - 1];
     while (true) {
-      if (c == pat[state]) {
-        ++state;
-        if (state == n) {
-          res = true;
-          state = pre[state - 1];
-        }
+      if (pat[i] == pat[pre[i]]) {
+        ++pre[i];
         break;
       }
-      if (state == 0)
+      if (pre[i] == 0)
         break;
-      state = pre[state - 1];
+      pre[i] = pre[pre[i] - 1];
     }
-    return res;
   }
+  state = 0;
+}
 
-}  // namespace kmp
+bool match(char c) {
+  bool res = false;
+  while (true) {
+    if (c == pat[state]) {
+      ++state;
+      if (state == n) {
+        res = true;
+        state = pre[state - 1];
+      }
+      break;
+    }
+    if (state == 0)
+      break;
+    state = pre[state - 1];
+  }
+  return res;
+}
+
+} // namespace kmp
 
 namespace ac {
 
-  struct Trie {
-    Trie() {
-      memset(children, 0, sizeof(children));
-      pre = 0;
-      word = false;
-    }
-    int children[26];
-    int pre;
-    bool word;
-  };
-
-  vector<Trie> tries;
-  int state;
-
-  void add(const string& s) {
-    int cur = 0;
-    for (size_t i = 0; i < s.size(); ++i) {
-      if (tries[cur].children[s[i] - 'a'] == 0) {
-        tries[cur].children[s[i] - 'a'] = tries.size();
-        tries.push_back(Trie());
-      }
-      cur = tries[cur].children[s[i] - 'a'];
-      tries[cur].word |= (i == s.size() - 1);
-    }
+struct Trie {
+  Trie() {
+    memset(children, 0, sizeof(children));
+    pre = 0;
+    word = false;
   }
+  int children[26];
+  int pre;
+  bool word;
+};
 
-  void build() {
-    vector<int> bfs = {0};
-    while (!bfs.empty()) {
-      vector<int> subs;
-      for (int cur : bfs) {
-        for (int i = 0; i < 26; ++i) {
-          if (tries[cur].children[i] == 0)
-            continue;
-          int nxt = tries[cur].children[i];
-          int pre = tries[cur].pre;
-          if (cur != 0) {
-            while (true) {
-              if (tries[pre].children[i] != 0) {
-                tries[nxt].pre = tries[pre].children[i];
-                break;
-              }
-              if (pre == 0)
-                break;
-              pre = tries[pre].pre;
+vector<Trie> tries;
+int state;
+
+void add(const string &s) {
+  int cur = 0;
+  for (size_t i = 0; i < s.size(); ++i) {
+    if (tries[cur].children[s[i] - 'a'] == 0) {
+      tries[cur].children[s[i] - 'a'] = tries.size();
+      tries.push_back(Trie());
+    }
+    cur = tries[cur].children[s[i] - 'a'];
+    tries[cur].word |= (i == s.size() - 1);
+  }
+}
+
+void build() {
+  vector<int> bfs = {0};
+  while (!bfs.empty()) {
+    vector<int> subs;
+    for (int cur : bfs) {
+      for (int i = 0; i < 26; ++i) {
+        if (tries[cur].children[i] == 0)
+          continue;
+        int nxt = tries[cur].children[i];
+        int pre = tries[cur].pre;
+        if (cur != 0) {
+          while (true) {
+            if (tries[pre].children[i] != 0) {
+              tries[nxt].pre = tries[pre].children[i];
+              break;
             }
+            if (pre == 0)
+              break;
+            pre = tries[pre].pre;
           }
-          subs.push_back(nxt);
         }
+        subs.push_back(nxt);
       }
-      swap(subs, bfs);
     }
+    swap(subs, bfs);
   }
+}
 
-  bool match(char c) {
-    while (true) {
-      if (tries[state].children[c - 'a'] != 0) {
-        state = tries[state].children[c - 'a'];
-        break;
-      }
-      if (state == 0)
-        break;
-      state = tries[state].pre;
+bool match(char c) {
+  while (true) {
+    if (tries[state].children[c - 'a'] != 0) {
+      state = tries[state].children[c - 'a'];
+      break;
     }
-    int cur = state;
-    while (cur) {
-      if (tries[cur].word)
-        return true;
-      cur = tries[cur].pre;
-    }
-    return false;
+    if (state == 0)
+      break;
+    state = tries[state].pre;
   }
+  int cur = state;
+  while (cur) {
+    if (tries[cur].word)
+      return true;
+    cur = tries[cur].pre;
+  }
+  return false;
+}
 
-}  // namespace ac
+} // namespace ac
 
 int main() {
   for (int i = 0; i < 100; ++i) {
