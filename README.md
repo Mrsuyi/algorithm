9b0e7a18fcd3296f40d2f65160f25c81465a1aa0
Mrsuyi <mrsuyi@163.com>
2020-11-17 17:41:50 -0800

    format md

--- README.md
+++ README.md
@@ -1,111 +1,53 @@
-# Templates
+\#Templates
 
-## Bit
-+ Get last "1" of n
+\#Bit + Get last "1" of n
+
 ```cpp
 n = k & -k;
 ```
 
-+ Remove last "1" from n
+*   Remove last "1" from n
+
 ```cpp
 n = k & (k - 1);
 ```
 
-+ Traverse subsets of k (e.g. k=5 -> {101, 100, 001})
-  ```cpp
-  for (int i = k; i; i = (i - 1) & k) {}
-  ```
+*   Traverse subsets of k(e.g.k = 5->{101, 100, 001})
 
-+ Binary indexed tree (Fenwick tree)
-  ```cpp
-  const int N = 1 << 20;
-  int arr[N] = {0};
-
-void add(int i, int val) {
-  while (i < N) {
-    arr[i] += val;
-    i += i & -i;
-  }
-}
-int sum(int i) {
-  int res = 0;
-  while (i) {
-    res += arr[i];
-    i = i & (i - 1);
-  }
-  return res;
-}
-```
-
-## Math
-
-+ GCD (Greatest Common Divisor)
-  ```cpp
-  // a >= b > 0
-  int gcd(int a, int b) {
-    return b ? gcd(b, a % b) : a;
-  }
-
-// Extended GCD: a*x+b*y=gcd(a, b)
-void exgcd(int a, int b, int& d, int& x, int& y) {
-  if (b) {
-    exgcd(b, a % b, d, y, x);
-    y -= x * (a / b);
-  } else {
-    d = a, x = 1, y = 0;
-  }
-}
-```
-
-+ Prime numbers
 ```cpp
-// Eratosthenes filtering
-// O(NloglogN)
-bool not_prime[100000] = {0};
-int primes[100000];
-int eratosthenes_filtering(int n) {
-  int tot = 0;
-  for (int i = 2; i <= n; ++i) {
-    if (not_prime[i])
-      continue;
-    primes[tot++] = i;
-    for (int j = i * i; j <= n; j += i)
-      not_prime[j] = true;
-  }
-  return tot;
-}
-
-// Euler filtering
-// O(N)
-bool not_prime[100000] = {0};
-int primes[100000];
-int euler_filtering(int n) {
-  int tot = 0;
-  for (int i = 2; i <= n; ++i) {
-    if (!not_prime[i])
-      primes[tot++] = i;
-    for (int j = 0; j < tot; ++j) {
-      if (i * primes[j] > n)
-        break;
-      not_prime[i * primes[j]] = true;
-      if (i % primes[j] == 0)
-        break;
-    }
-  }
-  return tot;
-}
-
-// Hash util
-namespace std {
-  template <> struct hash<vector<int>> {
-    size_t operator()(const vector<int> &vec) const {
-      size_t seed = 0;
-      for (size_t v : vec) {
-        seed ^= v + 0x9e3779b9 + (seed << 6) + (seed >> 2);
-      }
-      return seed;
-    }
-  };
-} // namespace std
+for (int i = k; i; i = (i - 1) & k) {}
 ```
 
+*   Binary indexed tree(Fenwick tree)
+
+```cpp
+// [0, ma)
+struct FenwickTree {
+  FenwickTree(int ma) : val(ma, 0) {}
+
+  void add(int i) {
+    if (i == 0) {
+      ++val[0];
+      return;
+    }
+    int ma = val.size();
+    while (i < ma) {
+      ++val[i];
+      i += i & -i;
+    }
+  }
+
+  int get(int i) {
+    int res = 0;
+    while (true) {
+      res += val[i];
+      if (i == 0)
+        break;
+      i &= i - 1;
+    }
+    return res;
+  }
+
+  vector<int> val;
+};
+```
--- templates/math.cpp
+++ templates/math.cpp
@@ -2,9 +2,11 @@
 #include <vector>
 using namespace std;
 
+// Custom hash function for vector.
 namespace std {
-template <> struct hash<vector<int>> {
-  size_t operator()(const vector<int> &vec) const {
+template <>
+struct hash<vector<int>> {
+  size_t operator()(const vector<int>& vec) const {
     size_t seed = 0;
     for (size_t v : vec) {
       seed ^= v + 0x9e3779b9 + (seed << 6) + (seed >> 2);
@@ -12,71 +14,66 @@ template <> struct hash<vector<int>> {
     return seed;
   }
 };
-} // namespace std
+}  // namespace std
 
-namespace brute_force {
+// Euclidean algorithm. a >= b.
+int gcd(int a, int b) { return (b == 0) ? a : gcd(b, a % b); }
 
+// Extended Euclidean algorithm.
+void exgcd(int a, int b, int& d, int& x, int& y) {
+  if (b) {
+    exgcd(b, a % b, d, y, x);
+    y -= x * (a / b);
+  } else {
+    d = a, x = 1, y = 0;
+  }
+}
+
+// Calculate prime numbers.
 vector<int> brute_force(int n) {
   vector<int> primes;
   primes.reserve(n / 10 + 500);
   for (int i = 2; i <= n; ++i) {
     int j = 2, k = sqrt(i);
-    while ((j <= k) && (i % j))
-      ++j;
-    if (j > k)
-      primes.push_back(i);
+    while ((j <= k) && (i % j)) ++j;
+    if (j > k) primes.push_back(i);
   }
   return primes;
 }
-
-} // namespace brute_force
-
-namespace euler {
 
 vector<int> euler_filtering(int n) {
   vector<bool> not_prime(n + 1, false);
   vector<int> primes;
   primes.reserve(n / 10 + 500);
   for (int i = 2; i <= n; ++i) {
-    if (!not_prime[i])
-      primes.push_back(i);
+    if (!not_prime[i]) primes.push_back(i);
     for (size_t j = 0; j < primes.size(); ++j) {
-      if (i * primes[j] > n)
-        break;
+      if (i * primes[j] > n) break;
       not_prime[i * primes[j]] = true;
-      if (i % primes[j] == 0)
-        break;
+      if (i % primes[j] == 0) break;
     }
   }
   return primes;
 }
-
-} // namespace euler
-
-namespace eratosthenes {
 
 vector<int> eratosthenes_filtering(int n) {
   vector<bool> not_prime(n + 1, false);
   vector<int> primes;
   primes.reserve(n / 10 + 500);
   for (long long i = 2; i <= n; ++i) {
-    if (not_prime[i])
-      continue;
+    if (not_prime[i]) continue;
     primes.push_back(i);
-    for (long long j = i * i; j <= n; j += i)
-      not_prime[j] = true;
+    for (long long j = i * i; j <= n; j += i) not_prime[j] = true;
   }
   return primes;
 }
 
-} // namespace eratosthenes
-
 int main() {
   for (int i = 0; i < 10; ++i) {
     int n = rand() % 100000;
-    vector<int> a = brute_force::brute_force(n);
-    vector<int> b = euler::euler_filtering(n);
-    vector<int> c = eratosthenes::eratosthenes_filtering(n);
+    vector<int> a = brute_force(n);
+    vector<int> b = euler_filtering(n);
+    vector<int> c = eratosthenes_filtering(n);
     assert(a == b);
     assert(b == c);
     printf("Number of primes under %d: %zu\n", n, a.size());
