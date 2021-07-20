81192ec14d8b102b2cd373de06d10a424349564f
Mrsuyi <mrsuyi@163.com>
2021-07-19 18:20:36 -0700

    kmp

--- templates/str.cpp
+++ templates/str.cpp
@@ -5,156 +5,144 @@
 #include <vector>
 using namespace std;
 
-namespace kmp {
-
-int n;
-string pat;
-vector<int> pre;
-int state;
-
-void init(const string &pattern) {
-  pat = pattern;
-  n = pat.size();
-  pre = vector<int>(n, 0);
-  for (int i = 1; i < n; ++i) {
-    pre[i] = pre[i - 1];
-    while (true) {
-      if (pat[i] == pat[pre[i]]) {
-        ++pre[i];
-        break;
-      }
-      if (pre[i] == 0)
-        break;
-      pre[i] = pre[pre[i] - 1];
-    }
-  }
-  state = 0;
-}
-
-bool match(char c) {
-  bool res = false;
-  while (true) {
-    if (c == pat[state]) {
-      ++state;
-      if (state == n) {
-        res = true;
-        state = pre[state - 1];
-      }
-      break;
-    }
-    if (state == 0)
-      break;
-    state = pre[state - 1];
-  }
+string rand_str(int len, int chr = 26) {
+  string res(len, '\0');
+  for (int i = 0; i < len; ++i)
+    res[i] = 'a' + (rand() % chr);
   return res;
 }
 
-} // namespace kmp
+class Kmp {
+  public:
+    Kmp(const string& pattern) : p(pattern), f(p.size() + 1, 0) {
+      for (int i = 1; i < p.size(); ++i) {
+        int j = f[i];
+        while (j && p[i] != p[j])
+          j = f[j];
+        if (p[i] == p[j])
+          f[i + 1] = j + 1;
+      }
+    }
+
+    int match(const string& s) {
+      int j = 0;
+      for (int i = 0; i < s.size(); ++i) {
+        while (j && s[i] != p[j])
+          j = f[j];
+        if (s[i] == p[j]) {
+          if (++j == p.size())
+            return i - p.size() + 1;
+        }
+      }
+      return -1;
+    }
+
+    // common[i] is the length of longest common prefix&suffix of pattern[0,i].
+    string p;
+    vector<int> f;
+};
+
+void test_kmp() {
+  srand(time(NULL));
+  for (int i = 0; i < 100; ++i) {
+    string p = rand_str(rand() % 10 + 1, 5);
+    string s = rand_str(rand() % 100 + 1, 5);
+    int kmp = Kmp(p).match(s);
+
+    int bf = -1;
+    for (int i = 0; i + p.size() <= s.size(); ++i) {
+      int j = 0;
+      for (; j < p.size(); ++j) {
+        if (s[i + j] != p[j])
+          break;
+      }
+      if (j == p.size()) {
+        bf = i;
+        break;
+      }
+    }
+    assert(bf == kmp);
+  }
+}
 
 namespace ac {
 
-struct Trie {
-  Trie() {
-    memset(children, 0, sizeof(children));
-    pre = 0;
-    word = false;
-  }
-  int children[26];
-  int pre;
-  bool word;
-};
-
-vector<Trie> tries;
-int state;
-
-void add(const string &s) {
-  int cur = 0;
-  for (size_t i = 0; i < s.size(); ++i) {
-    if (tries[cur].children[s[i] - 'a'] == 0) {
-      tries[cur].children[s[i] - 'a'] = tries.size();
-      tries.push_back(Trie());
+  struct Trie {
+    Trie() {
+      memset(children, 0, sizeof(children));
+      pre = 0;
+      word = false;
     }
-    cur = tries[cur].children[s[i] - 'a'];
-    tries[cur].word |= (i == s.size() - 1);
-  }
-}
+    int children[26];
+    int pre;
+    bool word;
+  };
 
-void build() {
-  vector<int> bfs = {0};
-  while (!bfs.empty()) {
-    vector<int> subs;
-    for (int cur : bfs) {
-      for (int i = 0; i < 26; ++i) {
-        if (tries[cur].children[i] == 0)
-          continue;
-        int nxt = tries[cur].children[i];
-        int pre = tries[cur].pre;
-        if (cur != 0) {
-          while (true) {
-            if (tries[pre].children[i] != 0) {
-              tries[nxt].pre = tries[pre].children[i];
-              break;
-            }
-            if (pre == 0)
-              break;
-            pre = tries[pre].pre;
-          }
-        }
-        subs.push_back(nxt);
+  vector<Trie> tries;
+  int state;
+
+  void add(const string &s) {
+    int cur = 0;
+    for (size_t i = 0; i < s.size(); ++i) {
+      if (tries[cur].children[s[i] - 'a'] == 0) {
+        tries[cur].children[s[i] - 'a'] = tries.size();
+        tries.push_back(Trie());
       }
+      cur = tries[cur].children[s[i] - 'a'];
+      tries[cur].word |= (i == s.size() - 1);
     }
-    swap(subs, bfs);
   }
-}
 
-bool match(char c) {
-  while (true) {
-    if (tries[state].children[c - 'a'] != 0) {
-      state = tries[state].children[c - 'a'];
-      break;
+  void build() {
+    vector<int> bfs = {0};
+    while (!bfs.empty()) {
+      vector<int> subs;
+      for (int cur : bfs) {
+        for (int i = 0; i < 26; ++i) {
+          if (tries[cur].children[i] == 0)
+            continue;
+          int nxt = tries[cur].children[i];
+          int pre = tries[cur].pre;
+          if (cur != 0) {
+            while (true) {
+              if (tries[pre].children[i] != 0) {
+                tries[nxt].pre = tries[pre].children[i];
+                break;
+              }
+              if (pre == 0)
+                break;
+              pre = tries[pre].pre;
+            }
+          }
+          subs.push_back(nxt);
+        }
+      }
+      swap(subs, bfs);
     }
-    if (state == 0)
-      break;
-    state = tries[state].pre;
   }
-  int cur = state;
-  while (cur) {
-    if (tries[cur].word)
-      return true;
-    cur = tries[cur].pre;
+
+  bool match(char c) {
+    while (true) {
+      if (tries[state].children[c - 'a'] != 0) {
+        state = tries[state].children[c - 'a'];
+        break;
+      }
+      if (state == 0)
+        break;
+      state = tries[state].pre;
+    }
+    int cur = state;
+    while (cur) {
+      if (tries[cur].word)
+        return true;
+      cur = tries[cur].pre;
+    }
+    return false;
   }
-  return false;
-}
 
 } // namespace ac
 
 int main() {
-  for (int i = 0; i < 100; ++i) {
-    int n = rand() % 9 + 1;
-    string pat(n, 0);
-    for (int j = 0; j < n; ++j)
-      pat[j] = 'a' + (rand() % 4);
-    kmp::init(pat);
-
-    int l = rand() % 100000 + 1;
-    string s(l, 0);
-
-    int ckmp = 0, cbf = 0;
-    for (int j = 0; j < l; ++j) {
-      s[j] = 'a' + (rand() % 4);
-
-      ckmp += kmp::match(s[j]);
-      bool bf = (j + 1) >= n;
-      for (int k = 0; k < n; ++k) {
-        if (s[j - k] != pat[n - 1 - k]) {
-          bf = false;
-          break;
-        }
-      }
-      cbf += bf;
-      assert(ckmp == cbf);
-    }
-    printf("Pattern: %d, String: %d, Match: %d\n", n, l, ckmp);
-  }
+  test_kmp();
   return 0;
 }
